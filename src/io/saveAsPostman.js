f7865d62e1b8efaa8f092d0dd5e6fc908a0354f6
Lorenzo Hidalgo <lorenzohidalgogadea@gmail.com>
2023-07-21 13:24:14 +0200

    initial commit (#2)
    * initial commit
    * saveAsFile test improvements + linter
    * saveAsPostman unit test coverage
    * added actions and funding info
    * added code owner

---
+++ src/io/saveAsPostman.js
@@ -0,0 +1,79 @@
+const { Collection, ItemGroup, Item, Url } = require('postman-collection');
+
+const { mkdirSync, writeFileSync } = require('fs');
+const { resolve, join } = require('path');
+
+const buildItemList = (operations, rawUrl) =>
+  operations.map(
+    (op) =>
+      new Item({
+        name: op.operationName,
+        request: {
+          method: 'POST',
+          url: Url.parse(rawUrl),
+          body: {
+            mode: 'graphql',
+            graphql: {
+              query: op.operation,
+              variables: '',
+            },
+          },
+        },
+      }),
+  );
+
+const saveAsPostman = (destinationDirectory, parsedSchema, collectionName, rawUrl) => {
+  if (!parsedSchema.mutations && !parsedSchema.queries && !parsedSchema.subscriptions) {
+    // eslint-disable-next-line no-console
+    console.error('No operations found to be saved');
+    return;
+  }
+  
+  const newCollection = new Collection({
+    info: {
+      name: collectionName,
+    },
+  });
+
+  if (parsedSchema.mutations) {
+    newCollection.items.add(
+      new ItemGroup({
+        name: 'mutations',
+        item: buildItemList(parsedSchema.mutations, rawUrl),
+      }),
+    );
+  }
+
+  if (parsedSchema.queries) {
+    newCollection.items.add(
+      new ItemGroup({
+        name: 'queries',
+        item: buildItemList(parsedSchema.queries, rawUrl),
+      }),
+    );
+  }
+
+  if (parsedSchema.subscriptions) {
+    newCollection.items.add(
+      new ItemGroup({
+        name: 'subscriptions',
+        item: buildItemList(parsedSchema.subscriptions, rawUrl),
+      }),
+    );
+  }
+
+  try {
+    mkdirSync(resolve(destinationDirectory));
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err;
+  }
+
+  writeFileSync(
+    join(destinationDirectory, `./${collectionName}.postman_collection.json`),
+    JSON.stringify(newCollection, null, 2),
+  );
+};
+
+module.exports = {
+  saveAsPostman,
+};
---
+++ src/parsers/parseSchema.js
@@ -0,0 +1,49 @@
+const { formatter } = require('../formatter');
+const { buildOperation } = require('./helpers/buildOperation');
+const { MUTATION, QUERY, SUBSCRIPTION } = require('./helpers/wrappers');
+
+const mapOperation = async (operationName, raw) => {
+  const operation = await formatter(raw);
+  return { operationName, operation };
+};
+
+const parseOperations = (gqlSchema, operationType, wrapper, maxDepth) => {
+  const operationFields = operationType.getFields();
+  return Object.keys(operationFields).map((opName) => {
+    const raw = wrapper(opName, buildOperation(gqlSchema, operationFields[opName], maxDepth));
+    return mapOperation(opName, raw);
+  });
+};
+
+const parseSchema = async (gqlSchema, maxDepth) => {
+  const parsedSchema = {};
+  if (gqlSchema.getMutationType()) {
+    parsedSchema.mutations = await Promise.all(
+      parseOperations(gqlSchema, gqlSchema.getMutationType(), MUTATION, maxDepth),
+    );
+  } else {
+    console.warn('No mutation type found in your schema');
+  }
+
+  if (gqlSchema.getQueryType()) {
+    parsedSchema.queries = await Promise.all(
+      parseOperations(gqlSchema, gqlSchema.getQueryType(), QUERY, maxDepth),
+    );
+  } else {
+    console.warn('No queries type found in your schema');
+  }
+
+  if (gqlSchema.getSubscriptionType()) {
+    parsedSchema.subscriptions = await Promise.all(
+      parseOperations(gqlSchema, gqlSchema.getSubscriptionType(), SUBSCRIPTION, maxDepth),
+    );
+  } else {
+    console.warn('No subscriptions type found in your schema');
+  }
+
+  return parsedSchema;
+};
+
+module.exports = {
+  parseSchema,
+};
